bc3c09092b03cf505c76f81e568a0d1a35248d39
Kalle Hallivuori <kato@iki.fi>
2014-11-24 22:20:40 +0200

    Fix command listing

--- command/settings.py
+++ command/settings.py
@@ -2,6 +2,9 @@ from command import Command
 
 
 class Settings(Command):
+    """
+    Show currently configured settings.
+    """
     def run(self):
         print('Current settings:')
         for setting in self.settings.values():
